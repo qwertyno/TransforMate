da7d7dab6af648955ce2be9de27aca1f9c39c8ab
Virak X64 <virakx64@qwerty.no>
2017-01-06 12:40:05 +0100

    Created project

---
+++ Source/Qwerty.VisualStudio.TransforMate/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TransforMate")]
+[assembly: AssemblyDescription("Visual Studio Extention for creating and maintaining config transformations")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Qwerty")]
+[assembly: AssemblyProduct("TransforMate")]
+[assembly: AssemblyCopyright("Copyright © 2017 by Qwerty. All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
